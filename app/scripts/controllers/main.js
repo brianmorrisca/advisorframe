82f500d3edb155d5be444d3c1e3ecffea42d8af2
bkmorris <bkmorris8@gmail.com>
2014-05-17 00:17:08 -0400

    check if value input is a number

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,31 +3,35 @@
 angular.module('advisorframeApp')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.securities = []
     $scope.etfs = [];
     $scope.commons = [];
     $scope.mfs = [];
 
     $scope.addSecurity = function () {
-
-        var radioType = $scope.securityType;
-
-        if (radioType == "etf"){
-            $scope.etfs.push({name:$scope.securityName, amount:$scope.securityAmount});
-    		$scope.securityName = '';
-            $scope.securityAmount = '';
-    	}
-        else if (radioType == "common") {
-            $scope.commons.push({name:$scope.securityName, amount:$scope.securityAmount});
-            $scope.securityName = '';
-            $scope.securityAmount = '';
+        
+        if (isNaN($scope.securityAmount))
+        {
+            alert("Must input a number");
         }
-        else if (radioType == "mf") {
-            $scope.mfs.push({name:$scope.securityName, amount:$scope.securityAmount});
-            $scope.securityName = '';
-            $scope.securityAmount = '';
-        };
+        else {
+            var radioType = $scope.securityType;
 
+            if (radioType == "etf"){
+                $scope.etfs.push({name:$scope.securityName, amount:$scope.securityAmount});
+        		$scope.securityName = '';
+                $scope.securityAmount = '';
+        	}
+            else if (radioType == "common") {
+                $scope.commons.push({name:$scope.securityName, amount:$scope.securityAmount});
+                $scope.securityName = '';
+                $scope.securityAmount = '';
+            }
+            else if (radioType == "mf") {
+                $scope.mfs.push({name:$scope.securityName, amount:$scope.securityAmount});
+                $scope.securityName = '';
+                $scope.securityAmount = '';
+            };
+        };
     };       
 
   });
